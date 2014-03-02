cd6d79d580dcfc31e5ee86682079fc2154c12cac
Ben Falconer <ben@falconers.me.uk>
2014-03-02 00:34:51 +0000

    Added linux support

--- lib/game.py
+++ lib/game.py
@@ -1,19 +1,34 @@
-import win32api
-import win32con
+import os
 import time
+if os.name == 'nt':
+    import win32api
+    import win32con
+else:
+    from subprocess import Popen
 
 class Game:
 
-    keymap = {
-        'up': 0x30,
-        'down': 0x31,
-        'left': 0x32,
-        'right': 0x33,
-        'a': 0x34,
-        'b': 0x35,
-        'start': 0x36,
-        'select': 0x37
-    }
+    if os.name == 'nt':
+        keymap = {
+            'up': 0x30,
+            'down': 0x31,
+            'left': 0x32,
+            'right': 0x33,
+            'a': 0x34,
+            'b': 0x35,
+            'start': 0x36,
+            'select': 0x37
+        }
+    else:
+        keymap = {
+            'up': ["Up"],
+            'down': ["Down"],
+            'left': ["Left"],
+            'right': ["Right"],
+            'a': ["a"],
+            'b': ["b"],
+            'start': ["Return"]
+        }
 
     def get_valid_buttons(self):
         return [button for button in self.keymap.keys()]
@@ -25,6 +40,11 @@ class Game:
         return self.keymap[button]
 
     def push_button(self, button):
-        win32api.keybd_event(self.button_to_key(button), 0, 0, 0)
-        time.sleep(.15)
-        win32api.keybd_event(self.button_to_key(button), 0, win32con.KEYEVENTF_KEYUP, 0)
+        if os.name == 'nt':
+            win32api.keybd_event(self.button_to_key(button), 0, 0, 0)
+            time.sleep(.15)
+            win32api.keybd_event(self.button_to_key(button), 0, win32con.KEYEVENTF_KEYUP, 0)
+        else:
+            Popen(["xdotool", "keydown"] + self.button_to_key(button))
+            time.sleep(.15)
+            Popen(["xdotool", "keyup"] + self.button_to_key(button))
